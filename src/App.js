6e65df3b98421624ba4d8ad90672d200006925ee
Sapsanchik <99867200+Sapsanchik@users.noreply.github.com>
2023-12-12 19:49:17 +0700

    use tabs 2 true

--- src/App.js
+++ src/App.js
@@ -3,39 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-    return React.createElement(
-        'div',
-        {
-            className: 'App',
-        },
-        React.createElement(
-            'header',
-            {
-                className: 'App-header',
-            },
-            React.createElement('img', {
-                src: logo,
-                className: 'App-logo',
-                alt: 'logo',
-            }),
-            React.createElement(
-                'p',
-                null,
-                'Edit ',
-                React.createElement('code', null, 'src/App.js'),
-                ' and save to reload.',
-            ),
-            React.createElement(
-                'a',
-                {
-                    className: 'App-link',
-                    href: 'https://reactjs.org',
-                    target: '_blank',
-                    rel: 'noopener noreferrer',
-                },
-                'Learn React',
-            ),
-            new Date().getFullYear(),
-        ),
-    );
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			new Date().getFullYear(),
+		),
+	);
 };
